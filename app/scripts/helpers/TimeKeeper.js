0573589286dc6b00a14e9fb423e81111d8549315
terjeofnorway <terjeofnorway@gmail.com>
2016-12-07 20:28:07 +0100

    Added minutes and seconds method for getting binary array

--- app/scripts/helpers/TimeKeeper.js
+++ app/scripts/helpers/TimeKeeper.js
@@ -12,13 +12,22 @@ class TimeKeeper{
         return numberArray;
     }
 
-    getBinaryHourFromDate(date = this.currentDate){
+    getBinaryHoursFromDate(date = this.currentDate){
         let hourStringArray = this.splitNumber(date.getHours());
-
-
-
         return [hourStringArray[0].toString(2),hourStringArray[1].toString(2)];
     }
+
+    getBinaryMinutesFromDate(date = this.currentDate){
+        let minutesStringArray = this.splitNumber(date.getMinutes());
+        return [minutesStringArray[0].toString(2),minutesStringArray[1].toString(2)];
+    }
+
+    getBinarySecondsFromDate(date = this.currentDate){
+        let secondsStringArray = this.splitNumber(date.getSeconds());
+        return [secondsStringArray[0].toString(2),secondsStringArray[1].toString(2)];
+    }
+
+
 
     tick(){
         this.currentDate = new Date();
--- test/TimeKeeperSpec.js
+++ test/TimeKeeperSpec.js
@@ -18,9 +18,9 @@ describe('Testing the TimeKeeper', () =>{
         expect(timeKeeper.splitNumber(testNumber2)).toEqual([0,5]);
     })
 
-    it('returns the correct binary clock array', () => {
+    it('returns the correct binary clock hours', () => {
         let date1 = new Date(2017,0,1,14,32,33);
-        let binaryHours1 = timeKeeper.getBinaryHourFromDate(date1);
+        let binaryHours1 = timeKeeper.getBinaryHoursFromDate(date1);
         let binaryHoursControl1 = ['1','100'];
 
         expect(binaryHours1).toEqual(binaryHoursControl1);
@@ -28,11 +28,41 @@ describe('Testing the TimeKeeper', () =>{
 
 
         let date2 = new Date(2017,0,1,7,32,33);
-        let binaryHours2 = timeKeeper.getBinaryHourFromDate(date2);
+        let binaryHours2 = timeKeeper.getBinaryHoursFromDate(date2);
         let binaryHoursControl2 = ['0','111'];
 
         expect(binaryHours2).toEqual(binaryHoursControl2);
+    })
 
+    it('returns the correct binary clock minutes', () => {
+        let date1 = new Date(2017,0,1,14,32,33);
+        let binaryMinutes1 = timeKeeper.getBinaryMinutesFromDate(date1);
+        let binaryMinutesControl1 = ['11','10'];
+
+        expect(binaryMinutes1).toEqual(binaryMinutesControl1);
+
+
+
+        let date2 = new Date(2017,0,1,7,9,33);
+        let binaryMinutes2 = timeKeeper.getBinaryMinutesFromDate(date2);
+        let binaryMinutesControl2 = ['0','1001'];
+
+        expect(binaryMinutes2).toEqual(binaryMinutesControl2);
+    })
+
+    it('returns the correct binary clock seconds', () => {
+        let date1 = new Date(2017,0,1,14,32,59);
+        let binarySeconds1 = timeKeeper.getBinarySecondsFromDate(date1);
+        let binarySecondsControl1 = ['101','1001'];
+
+        expect(binarySeconds1).toEqual(binarySecondsControl1);
+
+
+        let date2 = new Date(2017,0,1,7,9,20);
+        let binarySeconds2 = timeKeeper.getBinarySecondsFromDate(date2);
+        let binarySecondsControl2 = ['10','0'];
+
+        expect(binarySeconds2).toEqual(binarySecondsControl2);
     })
 
 
